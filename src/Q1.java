79f37fb626693ba2be3e3bcb9e83c85fe4f21f4d
ethevev <valentin.etheve@etu.umontpellier.fr>
2020-02-26 10:57:45 +0100

    prog 1

--- src/Q1.java
+++ src/Q1.java
@@ -1,4 +1,19 @@
 public class Q1 {
 
+    public int[] calculCarre(int a, int b){
+
+        int[] tab = new int[];
+        int i =0;
+
+        for (int x =a ; x<=b ; x++){
+
+            tab[i] = (int) Math.sqrt(x) ;
+            i++;
+
+        }
+        return tab ;
+    }
+
+
 
 }
